d604df318bb940bd5c856d0d491372594c0634bb
mordergames <119002678+mordergames@users.noreply.github.com>
2023-03-10 19:54:15 +0300

    Delete Main21_23_2.java

--- Main21_23_2.java
+++
@@ -1,7 +0,0 @@
-public class Main21_23_2 {
-    public static void main(String[] args) {
-        String[] array = {"hello", "world", "this", "is", "a", "test"};
-        String longestString = Main21_23.findLongestString(array);
-        System.out.println("The longest string is: " + longestString);
-    }
-}
